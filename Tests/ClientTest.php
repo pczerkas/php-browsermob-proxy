37486aca8ef812e3b1c2c27e06f3f4a172e0133c
Chris Hartjes <chartjes@littlehart.net>
2013-11-13 11:51:01 -0500

    Fixes to match accepted coding standards for this project

--- Tests/ClientTest.php
+++ Tests/ClientTest.php
@@ -1,119 +1,258 @@
 <?php
 require '../vendor/autoload.php';
 
-class ServerTest extends PHPUnit_Framework_TestCase {
-  protected function setUp() {
-    $this->client = new PHPBrowserMobProxy_Client("localhost:8080");
-  }
+/**
+ * Tests BrowserMobProxy Client
+ *
+ * @class ServerTest
+ * @category Testing
+ * @package  PHPBrowserMobProxy
+ * @author   Adam Goucher <adam@element34.ca>
+ * @author   Chris Hartjes <chartjes@littlehart.net>
+ * @license  Apache 2.0
+ * @link     https://github.com/chartjes/PHPBrowserMobProxy
+ */
+class ServerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Setup method
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->client = new PHPBrowserMobProxy_Client("localhost:8080");
+    }
 
-  protected function tearDown() {
-    $this->client->close();
-  }
+    /**
+     * Tear-down method
+     *
+     * @return void
+     */
+    protected function tearDown()
+    {
+        $this->client->close();
+    }
 
-  public function testProxyExists() {
-    $r = $this->client->close();
-  }
+    /**
+     * Test if the proxy exists
+     *
+     * @return void
+     */
+    public function testProxyExists()
+    {
+        $r = $this->client->close();
+    }
 
-  public function testNamedHar() {
-    $this->client->new_har("google");
-  }
-  
-  public function testUnNamedHar() {
-    $this->client->new_har();
-  }
+    /**
+     * Test that labelling HAR files works
+     *
+     * @return void
+     */
+    public function testNamedHar()
+    {
+        $this->client->newHar("google");
+    }
 
-  public function testNamedPage() {
-    $this->client->new_har();
-    $this->client->new_page("foo");
-  }
+    /**
+     * Test that we can create an un-named HAR
+     *
+     * @return void
+     */
+    public function testUnNamedHar()
+    {
+        $this->client->newHar();
+    }
 
-  public function testUnNamedPage() {
-    $this->client->new_har("Aa");
-    $this->client->new_page("Bb");
-  }
+    /**
+     * Test we can create a named page
+     *
+     * @return void
+     */
+    public function testNamedPage()
+    {
+        $this->client->newHar();
+        $this->client->newPage("foo");
+    }
 
-  public function testGetHar() {
-    $this->client->new_har("Aa");
-    $this->client->new_page("Bb");
-    $h = $this->client->har;
-  }
+    /**
+     * Test we can create an un-named page
+     *
+     * @return void
+     */
+    public function testUnNamedPage()
+    {
+        $this->client->newHar("Aa");
+        $this->client->newPage("Bb");
+    }
 
-  public function testBlackList() {
-    $this->client->whitelist('.*\.doubleclick\.net', 200);
-    $this->client->new_har("noads");
-  }
+    /**
+     * Test we can retriver a named HAR
+     *
+     * @return void
+     */
+    public function testGetHar()
+    {
+        $this->client->newHar("Aa");
+        $this->client->newPage("Bb");
+        $h = $this->client->har;
+    }
 
-  public function testWhiteList() {
-    $this->client->whitelist('.*\.doubleclick\.net', 200);
-    $this->client->new_har("noads");
-  }
+    /**
+     * Test the blacklist functionality
+     *
+     * @return void
+     */
+    public function testBlackList()
+    {
+        $this->client->whitelist('.*\.doubleclick\.net', 200);
+        $this->client->newHar("noads");
+    }
 
-  public function testBasicAuth() {
-    $response = $this->client->basic_auth('yoyo.org', array('username' => 'foo', 'password' => 'bar'));
-    $this->assertEquals($response->status_code, 200);
-  }
+    /**
+     * Test the whitelist functionality
+     *
+     * @return void
+     */
+    public function testWhiteList()
+    {
+        $this->client->whitelist('.*\.doubleclick\.net', 200);
+        $this->client->newHar("noads");
+    }
 
-  public function testHeaders() {
-    $response = $this->client->headers(array('ribbit' => 'rabbit'));
-    $this->assertEquals($response->status_code, 200);
-  }
+    /**
+     * Test basic auth
+     *
+     * @return void
+     */
+    public function testBasicAuth()
+    {
+        $response = $this->client->basicAuth(
+            'yoyo.org',
+            array('username' => 'foo', 'password' => 'bar')
+        );
+        $this->assertEquals($response->status_code, 200);
+    }
 
-  public function testResponseInterceptor() {
-    $response = $this->client->response_interceptor('ffdskl');
-    $this->assertEquals($response->status_code, 200);
-  }
-  
-  public function testRequestInterceptor() {
-    $response = $this->client->request_interceptor('ffdskl');
-    $this->assertEquals($response->status_code, 200);
-  }
+    /**
+     * Test we send headers
+     *
+     * @return void
+     */
+    public function testHeaders()
+    {
+        $response = $this->client->headers(array('ribbit' => 'rabbit'));
+        $this->assertEquals($response->status_code, 200);
+    }
 
-  public function testUpstreamLimists() {
-    $limits = array (
-      "downstreamKbps" => 12,
-      "upstreamKbps" => 34,
-      "latency" => 3
-    );
-    $response = $this->client->limits($limits);
-    $this->assertEquals($response->status_code, 200);
-  }
+    /**
+     * Test that the response inteceptor works
+     *
+     * @return void
+     */
+    public function testResponseInterceptor()
+    {
+        $response = $this->client->responseInterceptor('ffdskl');
+        $this->assertEquals($response->status_code, 200);
+    }
 
-  public function testTimeouts() {
-    $timeouts = array (
-      "request" => 12,
-      "read" => 34,
-      "connection" => 3,
-      "dns" => 2
-    );
-    $response = $this->client->timeouts($timeouts);
-    $this->assertEquals($response->status_code, 200);
-  }
+    /**
+     * Test we can create a named page
+     *
+     * @return void
+     */
+    public function testRequestInterceptor()
+    {
+        $response = $this->client->requestInterceptor('ffdskl');
+        $this->assertEquals($response->status_code, 200);
+    }
 
-  public function testRemapHosts() {
-    $response = $this->client->remap_hosts("a.b.c", "d.e.f");
-    $this->assertEquals($response->status_code, 200);
-  }
+    /**
+     * Test we can set upstream limits
+     *
+     * @return void
+     */
+    public function testUpstreamLimits()
+    {
+        $limits = array (
+            "downstreamKbps" => 12,
+            "upstreamKbps" => 34,
+            "latency" => 3
+        );
+        $response = $this->client->limits($limits);
+        $this->assertEquals($response->status_code, 200);
+    }
 
-  public function testWaitForTrafficToStop() {
-    $response = $this->client->wait_for_traffic_to_stop(5, 30);
-    $this->assertEquals($response->status_code, 200);
-  }
+    /**
+     * Test we can set timeouts
+     *
+     * @return void
+     */
+    public function testTimeouts()
+    {
+        $timeouts = array (
+            "request" => 12,
+            "read" => 34,
+            "connection" => 3,
+            "dns" => 2
+        );
+        $response = $this->client->timeouts($timeouts);
+        $this->assertEquals($response->status_code, 200);
+    }
 
-  public function testClearDNSCache() {
-    $response = $this->client->clear_dns_cache();
-    $this->assertEquals($response->status_code, 200);
-  }
+    /**
+     * Test we can remap hosts
+     *
+     * @return void
+     */
+    public function testRemapHosts()
+    {
+        $response = $this->client->remapHosts("a.b.c", "d.e.f");
+        $this->assertEquals($response->status_code, 200);
+    }
 
-  public function testRewriteURL() {
-    $response = $this->client->rewrite_url('foo', 'bar');
-    $this->assertEquals($response->status_code, 200);
-  }
+    /**
+     * Test we can set traffic stop times
+     *
+     * @return void
+     */
+    public function testWaitForTrafficToStop()
+    {
+        $response = $this->client->waitForTrafficToStop(5, 30);
+        $this->assertEquals($response->status_code, 200);
+    }
 
-  public function testRetry() {
-    $response = $this->client->retry(3);
-    $this->assertEquals($response->status_code, 200);
-  }
+    /**
+     * Test we can clear the proxy's DNS cache
+     *
+     * @return void
+     */
+    public function testClearDNSCache()
+    {
+        $response = $this->client->clearDnsCache();
+        $this->assertEquals($response->status_code, 200);
+    }
 
+    /**
+     * Test we can rewrite URL's inside the proxy
+     *
+     * @return void
+     */
+    public function testRewriteURL()
+    {
+        $response = $this->client->rewriteUrl('foo', 'bar');
+        $this->assertEquals($response->status_code, 200);
+    }
+
+    /**
+     * Test we can set a retry time for the proxy
+     *
+     * @return void
+     */
+    public function testRetry()
+    {
+        $response = $this->client->retry(3);
+        $this->assertEquals($response->status_code, 200);
+    }
 }
-
 ?>
--- src/PHPBrowserMobProxy/Client.php
+++ src/PHPBrowserMobProxy/Client.php
@@ -1,195 +1,421 @@
 <?php
 
-//require_once 'Requests.php';
-//Requests::register_autoloader();
+/**
+ * Class for interacting with BrowserMobProxy when using PHP
+ * Relies on the Requests library (https://github.com/rmccue/Requests)
+ *
+ * @class PHPBrowserMobProxy_Client
+ * @category Testing
+ * @package  PHPBrowserMobProxy
+ * @author   Adam Goucher <adam@element34.ca>
+ * @author   Chris Hartjes <chartjes@littlehart.net>
+ * @license  Apache 2.0
+ * @link     https://github.com/chartjes/PHPBrowserMobProxy
+ */
+class PHPBrowserMobProxy_Client
+{
+    /**
+     * Class constructor
+     *
+     * @param string $url URL for BrowserMobProxy instance
+     */
+    public function __construct($url)
+    {
+        $this->browsermob_url = $url;
 
-class PHPBrowserMobProxy_Client {
-  function __construct($url) {
-    $this->browsermob_url = $url;
+        $parts = parse_url($this->browsermob_url);
+        $this->hostname = $parts["host"];
 
-    $parts = parse_url($this->browsermob_url);
-    $this->hostname = $parts["host"];
-    
-    $response = Requests::post("http://" . $this->browsermob_url . "/proxy/");
-    
-    $decoded = json_decode($response->body, true);
-    if ($decoded) {
-        $this->port = $decoded["port"];
+        $response = Requests::post("http://" . $this->browsermob_url . "/proxy/");
+
+        $decoded = json_decode($response->body, true);
+        if ($decoded) {
+            $this->port = $decoded["port"];
+        }
+        $this->url = $this->hostname . ":" . $this->port;
     }
-    $this->url = $this->hostname . ":" . $this->port;
-  }
-  
-  function close() {
-    $response = Requests::delete("http://" . $this->browsermob_url . "/" . $this->port);
-  }
-  
-  private function encode_array($args) {
-    if (!is_array($args)) {
-      return false;
+
+    /**
+     * Close connection to the proxy
+     *
+     * @return void
+     */
+    public function close()
+    {
+        $response = Requests::delete("http://{$this->browsermob_url}/{$this->port}");
     }
-    $c = 0;
-    $out = '';
-    foreach($args as $name => $value) {
-      if ($c++ != 0){
-        $out .= '&';
-      }
-      $out .= urlencode("$name").'=';
-      if (is_array($value)) {
-        $out .= urlencode(serialize($value));
-      } else {
-        $out .= urlencode("$value");
-      }
+
+    /**
+     * Private method for encoding an array of arguments, used internally
+     *
+     * @param array $args array of arguments to URLencode
+     *
+     * @return boolean|string
+     */
+    private function _encodeArray($args)
+    {
+        if (!is_array($args)) {
+            return false;
+        }
+        $c = 0;
+        $out = '';
+
+        foreach ($args as $name => $value) {
+            if ($c++ != 0) {
+                $out .= '&';
+            }
+            $out .= urlencode("$name").'=';
+            if (is_array($value)) {
+                $out .= urlencode(serialize($value));
+            } else {
+                $out .= urlencode("$value");
+            }
+        }
+        return $out;
     }
-    return $out;
-  }
 
-  function __get($property) {
-    switch($property) {
-      case "har":
-        $proxy_handle = curl_init();
-        curl_setopt($proxy_handle, CURLOPT_URL, "http://" . $this->browsermob_url . "/proxy/" . $this->port . "/har");
-        curl_setopt($proxy_handle, CURLOPT_RETURNTRANSFER, True);
-        $result = curl_exec($proxy_handle);
-        $decoded = json_decode($result, true);
-        curl_close($proxy_handle);
-        return $decoded;
-      default:
-        return $this->$property;
+    /**
+     * Magic method for handling the returnig of properties
+     *
+     * @param string $property class property
+     *
+     * @return mixed
+     */
+    public function __get($property)
+    {
+        switch($property) {
+            case "har":
+                $proxy_handle = curl_init();
+                $har_url = "http://{$this->browsermob_url}/proxy/{$this->port}/har";
+                curl_setopt($proxy_handle, CURLOPT_URL, $har_url);
+                curl_setopt($proxy_handle, CURLOPT_RETURNTRANSFER, true);
+                $result = curl_exec($proxy_handle);
+                $decoded = json_decode($result, true);
+                curl_close($proxy_handle);
+                return $decoded;
+            default:
+                return $this->$property;
+        }
     }
-  }
 
-  function new_har($label = '') {
-    $data = "initialPageRef=" . $label;
-    $response = Requests::put("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/har",
-                              array(),
-                              $data);
-    return $response;
-  }
-
-  function new_page($label = '') {
-    $data = "pageRef=" . $label;
-    $response = Requests::put("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/har/pageRef",
-                              array(),
-                              $data);
-    return $response;
-  }
-
-  function blacklist($regexp, $status_code) {
-    $data = $this->encode_array(array("regex" => $regexp, "status" => $status_code));
-    $response = Requests::put("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/blacklist",
-                              array(),
-                              $data);
-    return $response;
-  }
-
-  function whitelist($regexp, $status_code) {
-    $data = $this->encode_array(array("regex" => $regexp, "status" => $status_code));
-    $response = Requests::put("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/whitelist",
-                              array(),
-                              $data);
-    return $response;
-  }
-
-  function basic_auth($domain, $options) {
-    $data = json_encode($options);
-    $response = Requests::post("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/auth/basic/" . $domain,
-                                array('Content-Type' => 'application/json'),
-                                $data);
-    return $response;
-  }
-
-  function headers($options) {
-    $data = json_encode($options);
-    $response = Requests::post("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/headers",
-                                array('Content-Type' => 'application/json'),
-                                $data);
-    return $response;
-  }
-
-  function response_interceptor($js) {
-    $response = Requests::post("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/interceptor/response",
-                            array('Content-Type' => 'x-www-form-urlencoded'),
-                            $js);
-    return $response;
-  }
-
-  function request_interceptor($js) {
-    $response = Requests::post("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/interceptor/request",
-                            array('Content-Type' => 'x-www-form-urlencoded'),
-                            $js);
-    return $response;
-  }
-
-  function limits($options) {
-    $keys = array("downstreamKbps" => "downstreamKbps",
-                  "upstreamKbps" => "upstreamKbps",
-                  "latency" => "latency");
-    foreach (array_keys($options) as $option_name) {
-      if (! array_key_exists($option_name, $keys)) {
-        throw new Exception($option_name . " is not a valid 'limits' option");
-      }
+    /**
+     * Method for creating a new HAR file
+     *
+     * @param string $label optional label
+     *
+     * @return string
+     */
+    public function newHar($label = '')
+    {
+        $data = "initialPageRef=" . $label;
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/har";
+        $response = Requests::put(
+            $url,
+            array(),
+            $data
+        );
+        return $response;
     }
-    $data = $this->encode_array($options);
-    $response = Requests::put("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/limit",
-                              array(),
-                              $data);
-    return $response;
-  }
-  
-  function timeouts($options) {
-    $keys = array("request" => "requestTimeout",
-                  "read" => "readTimeout",
-                  "connection" => "connectionTimeout",
-                  "dns" => "dnsCacheTimeout");
-    foreach (array_keys($options) as $option_name) {
-      if (! array_key_exists($option_name, $keys)) {
-        throw new Exception($option_name . " is not a valid 'timeouts' option");
-      }
+
+    /**
+     * Method for returning HAR info about one page
+     *
+     * @param string $label optional label for page
+     *
+     * @return string
+     */
+    public function newPage($label = '')
+    {
+        $data = "pageRef=" . $label;
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/har/pageRef";
+        $response = Requests::put(
+            $url,
+            array(),
+            $data
+        );
+        return $response;
     }
-    $data = $this->encode_array($options);
-    $response = Requests::put("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/timeout",
-                              array(),
-                              $data);
-    return $response;
-  }
 
-  function remap_hosts($address, $ip_address) {
-    $data = json_encode(array($address => $ip_address));
-    $response = Requests::post("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/hosts",
-                                array('Content-Type' => 'application/json'),
-                                $data);
-    return $response;
-  }
+    /**
+     * Add regex pattern to the proxy blacklist
+     *
+     * @param string  $regexp      regular expression
+     * @param integer $status_code HTTP status code
+     *
+     * @return string
+     */
+    public function blacklist($regexp, $status_code)
+    {
+        $data = $this->_encodeArray(
+            array(
+                "regex" => $regexp,
+                "status" => $status_code
+            )
+        );
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/blacklist";
+        $response = Requests::put(
+            $url,
+            array(),
+            $data
+        );
+        return $response;
+    }
 
-  function wait_for_traffic_to_stop($quiet_period, $timeout) {
-    $data = $this->encode_array(array('quietPeriodInMs' => (string)($quiet_period * 1000), 'timeoutInMs' => (string)($timeout * 1000)));
-    $response = Requests::put("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/wait",
-                              array(),
-                              $data);
-    return $response;
-  }
+    /**
+     * Add regex pattern to the proxy whitelist
+     *
+     * @param string  $regexp      regular expression
+     * @param integer $status_code HTTP status code
+     *
+     * @return string
+     */
+    public function whitelist($regexp, $status_code)
+    {
+        $data = $this->_encodeArray(
+            array(
+                "regex" => $regexp,
+                "status" => $status_code
+            )
+        );
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/whitelist";
+        $response = Requests::put(
+            $url,
+            array(),
+            $data
+        );
+        return $response;
+    }
 
-  function clear_dns_cache() {
-    $response = Requests::delete("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/dns/cache");
-    return $response;
-  }
+    /**
+     * Handle basic authentication using the proxy
+     *
+     * @param string $domain  domain name
+     * @param array  $options set of options for auth
+     *
+     * @return string
+     */
+    public function basicAuth($domain, $options)
+    {
+        $data = json_encode($options);
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/auth/basic/{$domain}";
+        $response = Requests::post(
+            $url,
+            array('Content-Type' => 'application/json'),
+            $data
+        );
+        return $response;
+    }
 
-  function rewrite_url($match, $replace) {
-    $data = $this->encode_array(array('matchRegex' => $match, 'replace' => $replace));
-    $response = Requests::put("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/rewrite",
-                              array(),
-                              $data);
-    return $response;
-  }
+    /**
+     * Send header information to the proxy
+     *
+     * @param array $options array of header options
+     *
+     * @return string
+     */
+    public function headers($options)
+    {
+        $data = json_encode($options);
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/headers";
+        $response = Requests::post(
+            $url,
+            array('Content-Type' => 'application/json'),
+            $data
+        );
+        return $response;
+    }
 
-  function retry($retry_count) {
-    $data = $this->encode_array(array('retrycount' => $retry_count));
-    $response = Requests::put("http://" . $this->browsermob_url . "/proxy/" . $this->port . "/retry",
-                              array(),
-                              $data);
-    return $response;
-  }
+    /**
+     * Send JavaScript to the response interceptor
+     *
+     * @param string $js JavaScript
+     *
+     * @return string
+     */
+    public function responseInterceptor($js)
+    {
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/interceptor/response";
+        $response = Requests::post(
+            $url,
+            array('Content-Type' => 'x-www-form-urlencoded'),
+            $js
+        );
+        return $response;
+    }
+
+    /**
+     * Send JavaScript to the request interceptor
+     *
+     * @param string $js JavaScript
+     *
+     * @return string
+     */
+    public function requestInterceptor($js)
+    {
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/interceptor/request";
+        $response = Requests::post(
+            $url,
+            array('Content-Type' => 'x-www-form-urlencoded'),
+            $js
+        );
+        return $response;
+    }
+
+    /**
+     * Set limits for the HTTP connection
+     *
+     * @param array $options options needed for limiting connections
+     *
+     * @return string
+     */
+    public function limits($options)
+    {
+        $keys = array(
+            "downstreamKbps" => "downstreamKbps",
+            "upstreamKbps" => "upstreamKbps",
+            "latency" => "latency"
+        );
+        foreach (array_keys($options) as $option_name) {
+            if (! array_key_exists($option_name, $keys)) {
+                throw new Exception($option_name . " is not a valid 'limits' option");
+            }
+        }
+        $data = $this->_encodeArray($options);
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/limit";
+        $response = Requests::put(
+            $url,
+            array(),
+            $data
+        );
+        return $response;
+    }
+
+    /**
+     * Set proxy timeouts
+     *
+     * @param array $options options needed for timeouts
+     *
+     * @return string
+     */
+    public function timeouts($options)
+    {
+        $keys = array(
+            "request" => "requestTimeout",
+            "read" => "readTimeout",
+            "connection" => "connectionTimeout",
+            "dns" => "dnsCacheTimeout"
+        );
+        foreach (array_keys($options) as $option_name) {
+            if (! array_key_exists($option_name, $keys)) {
+                throw new Exception($option_name . " is not a valid 'timeouts' option");
+            }
+        }
+        $data = $this->_encodeArray($options);
+        $url ="http://{$this->browsermob_url}/proxy/{$this->port}/timeout";
+        $response = Requests::put(
+            $url,
+            array(),
+            $data
+        );
+        return $response;
+    }
+
+    /**
+     * Method for remapping host names to IP addresses for local use
+     *
+     * @param string $address    URL to map
+     * @param string $ip_address IP address to map to URL
+     *
+     * @return string
+     */
+    public function remapHosts($address, $ip_address)
+    {
+        $data = json_encode(array($address => $ip_address));
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/hosts";
+        $response = Requests::post(
+            $url,
+            array('Content-Type' => 'application/json'),
+            $data
+        );
+        return $response;
+    }
+
+    /**
+     * Method for setting how long proxy should wait for traffic to stop
+     *
+     * @param integer $quiet_period time in milliseconds
+     * @param integer $timeout      time in milliseconds
+     *
+     * @return string
+     */
+    function waitForTrafficToStop($quiet_period, $timeout)
+    {
+        $data = $this->_encodeArray(
+            array(
+                'quietPeriodInMs' => (string)($quiet_period * 1000),
+                'timeoutInMs' => (string)($timeout * 1000)
+            )
+        );
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/wait";
+        $response = Requests::put(
+            $url,
+            array(),
+            $data
+        );
+        return $response;
+    }
+
+    /**
+     * Method for clearing the DNS cache set with remapHosts
+     *
+     * @return string
+     */
+    public function clearDnsCache()
+    {
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/dns/cache";
+        $response = Requests::delete($url);
+        return $response;
+    }
+
+    /**
+     * Method for rewriting URL's sent to the proxy
+     *
+     * @param string $match   pattern to match on
+     * @param string $replace string to replace matches with
+     *
+     * @return string
+     */
+    public function rewriteUrl($match, $replace)
+    {
+        $data = $this->_encodeArray(
+            array('matchRegex' => $match, 'replace' => $replace)
+        );
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/rewrite";
+        $response = Requests::put(
+            $url,
+            array(),
+            $data
+        );
+        return $response;
+    }
+
+    /**
+     * Method to set how many times a request should be retried
+     *
+     * @param integer $retry_count how many times to retry a request
+     *
+     * @return string
+     */
+    public function retry($retry_count)
+    {
+        $data = $this->_encodeArray(array('retrycount' => $retry_count));
+        $url = "http://{$this->browsermob_url}/proxy/{$this->port}/retry";
+        $response = Requests::put(
+            $url,
+            array(),
+            $data
+        );
+        return $response;
+    }
 }
-
-
 ?>
